82b80914fd7ff39ef51e188ae1f4cca01ea11dfa
WannesW <58140918+WannesW@users.noreply.github.com>
2021-11-23 22:02:24 +0100

    grid

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import FlipMove from 'react-flip-move'
 
 function Results({ results }) {
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5">
       {results.map(result => (
         <Thumbnail key={result.id} result={result} />
       ))}
